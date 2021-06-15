7839599a63bff5f3a48e15a34d69ba1da987d6fd
gourav <gouravk842@gmail.com>
2021-06-15 08:56:08 +0530

    csv creation Updated

--- CollegeManagementSystem/src/main/java/com/collegemanagementsystem/Controller/StudentAttendanceController.java
+++ CollegeManagementSystem/src/main/java/com/collegemanagementsystem/Controller/StudentAttendanceController.java
@@ -2,11 +2,14 @@ package com.collegemanagementsystem.Controller;
 
 import com.collegemanagementsystem.Dto.StudentAttendanceDto;
 import com.collegemanagementsystem.Service.AttendanceService;
+import com.collegemanagementsystem.Service.CsvGeneration;
+import com.collegemanagementsystem.Service.PdfGeneration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
@@ -16,21 +19,34 @@ public class StudentAttendanceController {
     @Autowired
     private AttendanceService attendanceService;
 
-    @RequestMapping(value="/takeAttendance",method = RequestMethod.POST)
+    @Autowired
+    private CsvGeneration csvGeneration;
+    @Autowired
+    private PdfGeneration pdfGeneration;
+
+    @RequestMapping(value = "/takeAttendance", method = RequestMethod.POST)
     @ResponseBody
-    public void takeAttendance(@RequestBody @Valid StudentAttendanceDto studentAttendanceDto){
+    public void takeAttendance(@RequestBody @Valid StudentAttendanceDto studentAttendanceDto) {
         attendanceService.takeAttendence(studentAttendanceDto);
     }
 
-    @RequestMapping(value="/getClassAttendance/{classId}",method = RequestMethod.GET)
+    @RequestMapping(value = "/getClassAttendance/{classId}", method = RequestMethod.GET)
     @ResponseBody
-    public Map getClassAttendance(@PathVariable(value = "classId") Long classId){
-       return attendanceService.getClassAttendance(classId);
+    public Map getClassAttendance(@PathVariable(value = "classId") Long classId) {
+        return attendanceService.getClassAttendance(classId);
     }
 
-    @RequestMapping(value="/getStudentAttendance/{classId}/{userId}",method = RequestMethod.GET)
+    @RequestMapping(value = "/getStudentAttendance/{classId}/{userId}", method = RequestMethod.GET)
     @ResponseBody
-    public Map getStudentAttendance(@PathVariable(value = "classId") Long classId,@PathVariable(value = "userId")Long userId){
-        return attendanceService.getStudentAttendance(userId,classId);
+    public Map getStudentAttendance(@PathVariable(value = "classId") Long classId, @PathVariable(value = "userId") Long userId) throws IOException {
+
+        pdfGeneration.generatePdf();
+        return attendanceService.getStudentAttendance(userId, classId);
+    }
+    @RequestMapping(value = "/getClassStudentAttendance/{classId}",method = RequestMethod.GET)
+    @ResponseBody
+    public Map getClassStudentAttendance(@PathVariable(value = "classId")Long classId){
+        attendanceService.classStudentAttendance(classId);
+        return null;
     }
 }
