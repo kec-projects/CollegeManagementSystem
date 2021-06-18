4e9aabc6ed07b888144c16b0bbd9a5717b531d97
AbhilashaKumari13 <abhilashakumari1308@gmail.com>
2021-06-18 13:17:51 +0530

    Online Application API

---
+++ CollegeManagementSystem/src/main/java/com/collegemanagementsystem/Dto/OnlineApplicationDto.java
@@ -0,0 +1,31 @@
+package com.collegemanagementsystem.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OnlineApplicationDto {
+
+    private Long userId;
+    private Long registrationNo;
+    private String name;
+    private String department;
+    private Long batch;
+    private String category;
+    private String fatherName;
+    private String motherName;
+    private Date dateOfBirth;
+    private String purpose;
+    private Date appliedDate;
+    private Date approvedDate;
+    private String certificateType;
+    private String status;
+    private String file;
+
+}
---
+++ CollegeManagementSystem/src/main/java/com/collegemanagementsystem/Entity/OnlineApplication.java
@@ -0,0 +1,55 @@
+package com.collegemanagementsystem.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Application")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OnlineApplication {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "SerialNo")
+    private Long sr;
+    @Column(name = "UserId")
+    private Long userId;
+    @Column(name = "RegistrationNo")
+    private Long registrationNo;
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "Department")
+    private String department;
+    @Column(name = "Batch")
+    private Long batch;
+    @Column(name = "Category")
+    private String category;
+    @Column(name = "FatherName")
+    private String fatherName;
+    @Column(name = "MotherName")
+    private String motherName;
+    @Column(name = "DateOfBirth")
+    private Date dateOfBirth;
+    @Column(name = "Purpose")
+    private String purpose;
+    @Column(name = "CertificateTaken")
+    private String certificateType;
+    @Column(name = "AppliedDate")
+    private Date appliedDate ;
+    @Column(name = "approvedDate")
+    private Date approvedDate;
+    @Column(name = "Status")
+    private String status;
+    private byte[] file;
+
+
+}
